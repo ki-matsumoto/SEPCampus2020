3c6d66ea144f4acea01efea298c3c9a0278ef033
kiyoaki matsumoto <matsumoto@alphaomega.co.jp>
2020-10-31 10:28:42 +0900

    辺り判定の改良

--- L04/Assets/ShapeCircle.cs
+++ L04/Assets/ShapeCircle.cs
@@ -40,31 +40,50 @@ class ShapeCircle : Shape
     override public bool HitTest(ShapeBox other)
     {
         Vector2 d = other.size / 2;
-        float x1 = other.transform.position.x - d.x;
-        float x2 = other.transform.position.x + d.x;
-        float y1 = other.transform.position.y - d.y;
-        float y2 = other.transform.position.y + d.y;
+        float nx = Mathf.Abs(transform.position.x - other.transform.position.x);
+        float ny = Mathf.Abs(transform.position.y - other.transform.position.y);
+        bool flag1 = nx < d.x;
+        bool flag2 = ny < d.y;
+        if (flag1 && flag2)
+        {
+            return true;
+        }
+        else if(flag2)
+        {
+            return nx < (radius + d.x);
+        }
+        else if (flag1)
+        {
+            return ny < (radius + d.y);
+        }
+        return  Mathf.Sqrt(nx * nx + ny * ny) <= radius + Mathf.Sqrt(d.x * d.x + d.y * d.y);
+    }
 
-        bool flag1 = x1 <= transform.position.x && transform.position.x <= x2;
-        bool flag2 = y1 <= transform.position.y && transform.position.y <= y2;
+#if false
+    override public bool HitTest(ShapeBox other)
+    {
+        Vector2 d = other.size / 2;
+        float nx = Mathf.Abs(transform.position.x - other.transform.position.x);
+        float ny = Mathf.Abs(transform.position.y - other.transform.position.y);
+        bool flag1 = nx < d.x;
+        bool flag2 = ny < d.y;
 
         if (flag1 && flag2)
         {
             return true;
         }
 
-        float nx = Mathf.Abs(transform.position.x - other.transform.position.x);
-        float ny = Mathf.Abs(transform.position.y - other.transform.position.y);
         if (flag2)
         {
             return (nx < (radius + other.size.x / 2));
         }
-        else if ( flag1 )
+        else if (flag1)
         {
             return (ny < (radius + other.size.y / 2));
         }
-        return  Mathf.Sqrt(nx * nx + ny * ny) <= Mathf.Sqrt(d.x * d.x + d.y * d.y) + radius;
+        return Mathf.Sqrt(nx * nx + ny * ny) <= radius + Mathf.Sqrt(d.x * d.x + d.y * d.y);
     }
+#endif
 
     private void OnDrawGizmos()
     {
--- L04/Assets/foo.cs
+++ L04/Assets/foo.cs
@@ -23,14 +23,19 @@ public class foo : MonoBehaviour
     private void OnDrawGizmos()
     {
 #if true
-        for (int i = 0; i < shapes.Count; i++)
+        for (int i = 0; i < shapes.Count-1; i++)
         {
             int no1 = i;
-            int no2 = i + 1;
-            if (no2 == shapes.Count) no2 = 0;
-            var clr1 = shapes[no1].HitTest(shapes[no2]) ? Color.red : Color.white;
-            Gizmos.color = clr1;
-            Gizmos.DrawLine(shapes[no1].transform.position, shapes[no2].transform.position);
+            for (int j = i+1; j < shapes.Count; j++)
+            {
+                int no2 = j;
+                bool hit = shapes[no1].HitTest(shapes[no2]);
+                if( hit )
+                {
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawLine(shapes[no1].transform.position, shapes[no2].transform.position);
+                }
+            }
 
             //text.text = string.Format("{0}", shapes[no1].transform.position.ToString());
         }
